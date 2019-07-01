7ad5d6a6461d177c8c03ace5cfb9d629076becc5
hamenok@note <zdrapalo@gmail.com>
2019-07-01 23:44:54 +0300

    Выполненно 2ое практическое задание

---
+++ lesson 2/js/script.js
@@ -0,0 +1,37 @@
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+    
+
+let appDate = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+for (let i=0; i<2; i++){
+    let exp1 = prompt("Введите обязательную статью расходов в этом месяце"),
+        exp2 = prompt("Во сколько обойдется?");
+    if (typeof(exp1) === 'string' && typeof(exp1) != null && 
+        typeof(exp2) != null && exp1 != '' && exp2 != '' && exp1.length < 50) {
+            appDate.expenses[exp1] = exp2;
+        } else {
+            i=0;
+        };
+};
+
+appDate.moneyPerDay = appDate.budget / 30;
+
+alert("Ежедневный бюджет: " + appDate.moneyPerDay);
+
+if (appDate.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+} else if (appDate.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+};
