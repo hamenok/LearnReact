122ba5dca4ee250bf603c31765d5b9c036932145
hamenok <zdrapalo@gmail.com>
2019-09-19 00:34:48 +0300

    037 Как работает setState()

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ important, done,
-      label, onToggleImportant, onToggleDone, onDelete }) => {
+export default class TodoListItem extends Component {
 
-  let classNames = 'todo-list-item';
-  if (important) {
-    classNames += ' important';
+  state = {
+    done: false,
+    important: false
+  };
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
+
+   
+
+
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={this.onLabelClick}>{label}</span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
+          <i className="fa fa-exclamation"></i>
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick="">
+          <i className="fa fa-trash-o"></i>
+        </button>
+      </span>
+    );
   }
-
-  if (done) {
-    classNames += ' done';
-  }
-
-
-  return (
-    <span className={classNames}>
-      <span
-        className="todo-list-item-label"
-        onClick={onToggleDone}>{label}</span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right"
-              onClick={onToggleImportant}>
-        <i className="fa fa-exclamation"></i>
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right"
-              onClick={onDelete}>
-        <i className="fa fa-trash-o"></i>
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+}
