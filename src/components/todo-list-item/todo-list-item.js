bdce472260ac799446a3e7aa443fc106cdf05246
hamenok <zdrapalo@gmail.com>
2019-09-21 00:19:18 +0300

    предфинад todo

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,26 +4,11 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  state = {
-    done: false,
-    important: false
-  };
-
-  onLabelClick = () => {
-    this.setState({
-      done: true
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState({
-      important: true
-    });
-  };
-
   render() {
-    const { label } = this.props;
-    const { done, important } = this.state;
+    const { label, onDeleted, 
+            onToggleImportant, onToggleDone,
+            done, important } = this.props;
+
 
     let classNames = 'todo-list-item';
 
@@ -35,24 +20,21 @@ export default class TodoListItem extends Component {
       classNames += ' important';
     }
 
-   
-
-
     return (
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          onClick={this.onLabelClick}>{label}</span>
+          onClick={onToggleDone}>{label}</span>
 
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-                onClick={this.onMarkImportant}>
+                onClick={onToggleImportant}>
           <i className="fa fa-exclamation"></i>
         </button>
 
         <button type="button"
                 className="btn btn-outline-danger btn-sm float-right"
-                onClick="">
+                onClick={onDeleted}>
           <i className="fa fa-trash-o"></i>
         </button>
       </span>
