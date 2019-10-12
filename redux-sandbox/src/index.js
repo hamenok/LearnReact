1320884afac7e0477cea6f4f2de3344e0326e92f
hamenok <Zdrapalo@gmail.com>
2019-10-13 01:58:44 +0300

    React + Redux

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, bindActionCreators } from 'redux';
+import * as actions from './actions';
+import reducer from './reducer';
+import Counter from './counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+const { dispatch } = store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+
+
+const update = () => {
+    ReactDOM.render(
+        <Counter 
+            counter={store.getState()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const value = Math.floor(Math.random()*10); 
+                rnd(value);
+            }} 
+        />, 
+    document.getElementById('root'));
+};
+
+update();
+store.subscribe(update);
+
