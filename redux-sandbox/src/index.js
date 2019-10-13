9402020237c30f106fbc276dd89f8bd34e3a98b8
hamenok <Zdrapalo@gmail.com>
2019-10-13 16:52:07 +0300

    функции redux

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
-import * as actions from './actions';
+import { createStore } from 'redux';
+
 import reducer from './reducer';
-import Counter from './counter';
+import Counter from './components/counter';
+import { Provider } from 'react-redux';
+import App from './components/app';
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-
-
-const update = () => {
     ReactDOM.render(
-        <Counter 
-            counter={store.getState()}
-            inc={inc}
-            dec={dec}
-            rnd={() => {
-                const value = Math.floor(Math.random()*10); 
-                rnd(value);
-            }} 
-        />, 
+        <Provider store={store}>
+            <App/>
+        </Provider>,
     document.getElementById('root'));
-};
-
-update();
-store.subscribe(update);
-
